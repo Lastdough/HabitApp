f4707b63c0d31e022393fc04eb225849d765032e
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-09 17:25:23 +0700

    Create a layout for pager_item and bind data to the view.  (See: Random Habit Screen specifications).

--- app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
@@ -3,6 +3,9 @@ package com.dicoding.habitapp.ui.random
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
@@ -39,9 +42,28 @@ class RandomHabitAdapter(
     inner class PagerViewHolder internal constructor(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
         //TODO 14 : Create view and bind data to item view
+        private val tvTitle = itemView.findViewById<TextView>(R.id.pager_tv_title)
+        private val tvStartTime = itemView.findViewById<TextView>(R.id.pager_tv_start_time)
+        private val tvMinutes = itemView.findViewById<TextView>(R.id.pager_tv_minutes)
+        private val ivPriority = itemView.findViewById<ImageView>(R.id.pager_priority_level)
+        private val btnOpenCountdown  = itemView.findViewById<Button>(R.id.btn_open_count_down)
 
         fun bind(pageType: PageType, pageData: Habit) {
+            tvTitle.text = pageData.title
+            tvStartTime.text = pageData.startTime
+            tvMinutes.text = pageData.minutesFocus.toString()
 
+            ivPriority.setImageResource(
+                when (pageType) {
+                    PageType.HIGH -> R.drawable.ic_priority_high
+                    PageType.MEDIUM -> R.drawable.ic_priority_medium
+                    PageType.LOW -> R.drawable.ic_priority_low
+                }
+            )
+
+            btnOpenCountdown.setOnClickListener {
+                onClick(pageData)
+            }
         }
     }
 }
