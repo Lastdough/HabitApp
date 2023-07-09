beaf94e3da71f8961c9257e9d2ddb2c8324468e4
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-09 17:26:19 +0700

    implement the NotificationWorker

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,24 +1,106 @@
 package com.dicoding.habitapp.notification
 
+    import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.countdown.CountDownActivity
+import com.dicoding.habitapp.ui.detail.DetailHabitActivity
+import com.dicoding.habitapp.ui.list.HabitListActivity
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
 
-class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+class NotificationWorker(private val ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
     private val habitId = inputData.getInt(HABIT_ID, 0)
     private val habitTitle = inputData.getString(HABIT_TITLE)
+    private val channelName = ctx.getString(R.string.notify_channel_name)
 
     override fun doWork(): Result {
-        val prefManager = androidx.preference.PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
+        val prefManager =
+            androidx.preference.PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val shouldNotify =
+            prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
 
         //TODO 12 : If notification preference on, show notification with pending intent
-
+        if (shouldNotify) {
+            sendNotification(ctx)
+        }
         return Result.success()
     }
+
+    private fun sendNotification(context: Context) {
+
+        val notificationIntent = Intent(context, HabitListActivity::class.java)
+
+        val taskStackBuilder: TaskStackBuilder = TaskStackBuilder.create(context)
+        taskStackBuilder.apply {
+            addParentStack(CountDownActivity::class.java)
+            addNextIntent(notificationIntent)
+        }
+
+
+        val pendingIntent = getPendingIntent(habitId)
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationSound =
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+
+        val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(habitTitle)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setSound(notificationSound)
+            .setContentText(context.getString(R.string.notify_content))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = channelName
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+
+        notificationManager.notify(0, notification)
+    }
+
+    private fun getPendingIntent(id: Int): PendingIntent? {
+        val intent = Intent(applicationContext, DetailHabitActivity::class.java).apply {
+            putExtra(HABIT_ID, id)
+        }
+        return TaskStackBuilder.create(applicationContext).run {
+            addNextIntentWithParentStack(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getPendingIntent(
+                    0,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            } else {
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+        }
+    }
+
 
 }
