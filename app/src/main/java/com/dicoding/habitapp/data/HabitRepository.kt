03efa4217d6a9ef1871b87baf5def4ed91642831
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-09 09:42:15 +0700

    Use SortUtils.getSortedQuery to create a sortable query

--- app/src/main/java/com/dicoding/habitapp/data/HabitRepository.kt
+++ app/src/main/java/com/dicoding/habitapp/data/HabitRepository.kt
@@ -2,22 +2,29 @@ package com.dicoding.habitapp.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.dicoding.habitapp.utils.HabitSortType
+import com.dicoding.habitapp.utils.SortUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class HabitRepository(private val habitDao: HabitDao, private val executor: ExecutorService) {
 
     companion object {
+        const val PAGE_SIZE = 30
+        const val PLACEHOLDERS = true
 
         @Volatile
         private var instance: HabitRepository? = null
 
         fun getInstance(context: Context): HabitRepository {
             return instance ?: synchronized(this) {
+                val applicationScope = CoroutineScope(SupervisorJob())
                 if (instance == null) {
-                    val database = HabitDatabase.getInstance(context)
+                    val database = HabitDatabase.getInstance(context, applicationScope)
                     instance = HabitRepository(
                         database.habitDao(),
                         Executors.newSingleThreadExecutor()
@@ -25,22 +32,28 @@ class HabitRepository(private val habitDao: HabitDao, private val executor: Exec
                 }
                 return instance as HabitRepository
             }
-
         }
     }
 
     //TODO 4 : Use SortUtils.getSortedQuery to create sortable query and build paged list
     fun getHabits(filter: HabitSortType): LiveData<PagedList<Habit>> {
-        throw NotImplementedError("Not yet implemented")
+        val filteredQuery = SortUtils.getSorteredQuery(filter)
+        val habits = habitDao.getHabits(filteredQuery)
+        val configuration = PagedList.Config.Builder()
+            .setEnablePlaceholders(PLACEHOLDERS)
+            .setInitialLoadSizeHint(PAGE_SIZE)
+            .setPageSize(PAGE_SIZE)
+            .build()
+        return LivePagedListBuilder(habits, configuration).build()
     }
 
     //TODO 5 : Complete other function inside repository
     fun getHabitById(habitId: Int): LiveData<Habit> {
-        throw NotImplementedError("Not yet implemented")
+        return habitDao.getHabitById(habitId)
     }
 
     fun insertHabit(newHabit: Habit) {
-        throw NotImplementedError("Not yet implemented")
+        habitDao.insertHabit(newHabit)
     }
 
     fun deleteHabit(habit: Habit) {
@@ -50,6 +63,6 @@ class HabitRepository(private val habitDao: HabitDao, private val executor: Exec
     }
 
     fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit> {
-        throw NotImplementedError("Not yet implemented")
+        return habitDao.getRandomHabitByPriorityLevel(level)
     }
 }
